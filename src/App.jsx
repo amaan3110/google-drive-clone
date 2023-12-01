f342b0865a9eb9c18efd73d0b6270199b7b0ccdd
amaan3110 <sheikhofficial48@gmail.com>
2023-12-01 18:12:28 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
-import Login from './loginPage/login'
+import './App.scss'
 import Screen from './screen/mainScreen'
 
 import {auth} from '../firebase'; 
+import { GoogleAuthProvider,signInWithPopup } from 'firebase/auth';
 import {useAuthState} from 'react-firebase-hooks/auth';
+
 
 const App = () => {
 
-  const [user] = useAuthState(auth); 
+  const provider = new GoogleAuthProvider();
+
+  const signIn = ()=>{
+    signInWithPopup(auth, provider)
+  }
+
+  const [user] = useAuthState(auth);
   return (
     <>
 
     {
-      user ? (<Login/>) : (<Screen/>)
+      user ? (<Screen/>) : 
+      (<button onClick={signIn}>Signin</button>)
     }
     
     
