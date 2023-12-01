e9e37b92dcd20d6fa32b6fe6023a3ef9ff5dd4f4
amaan3110 <sheikhofficial48@gmail.com>
2023-12-01 18:53:52 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.scss'
 import Screen from './screen/mainScreen'
+import 'boxicons'
 
 import {auth} from '../firebase'; 
 import { GoogleAuthProvider,signInWithPopup } from 'firebase/auth';
@@ -15,13 +16,33 @@ const App = () => {
     signInWithPopup(auth, provider)
   }
 
+  const createAccount = ()=>{
+    window.location.href = 'https://accounts.google.com/signup/v2/createaccount?theme=glif&flowName=GlifWebSignIn&flowEntry=SignUp';
+  }
+
   const [user] = useAuthState(auth);
   return (
     <>
 
     {
       user ? (<Screen/>) : 
-      (<button onClick={signIn}>Signin</button>)
+      (
+        <div className="login-box">
+          <img src="https://cdn.usbrandcolors.com/images/logos/google-logo.svg" alt="" />
+          <h2>Sign In</h2>
+          <p>to continue to Google Drive</p>
+          <button onClick={signIn}>
+          <box-icon type='logo' name='google'></box-icon>
+          <span>Signin with your Google Account</span>
+          </button>
+          <button onClick={signIn}>
+          <box-icon type='logo' name='github'></box-icon>
+          <span>Signin with your Github Account</span>
+          </button>
+          <p>Not your computer? Use Guest mode to sign in privately.</p>
+          <h4 onClick={createAccount}>Create Account</h4>
+        </div>
+      )
     }
     
     
